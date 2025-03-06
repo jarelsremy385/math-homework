f2cb1ca1824e03f4f4ff803dfe6a2ff8c5f6d6cd
jarelsremy385 <jarelsremy385@gmail.com>
2025-03-06 15:24:38 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,9 @@
+int main() {
+    // generate a random number between 1 and 10
+    int num = rand() % 10 + 1;
+
+    // print the number
+    cout << "Random number: " << num << endl;
+
+    return 0;
+}
