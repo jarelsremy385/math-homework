7860b5acec31915f02de0fcd5884477f8326693f
jarelsremy385 <jarelsremy385@gmail.com>
2025-03-10 00:46:13 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 2.0;
+    double y = -3.0;
+    double z = 1.5;
+    cout << "The value of the expression is: " << (x*y)/z << endl;
+    return 0;
+}
