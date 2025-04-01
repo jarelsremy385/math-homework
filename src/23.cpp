283cb18a6db59f2d5f45d51e6071a5b4f56783d6
jarelsremy385 <jarelsremy385@gmail.com>
2025-04-01 15:34:26 +0000

    Add 23.cpp

---
+++ src/23.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    float z = (x + y) / 2.0f;
+
+    cout << "The sum of x and y is: " << x << " + " << y << " = " << z << endl;
+
+    return 0;
+}
