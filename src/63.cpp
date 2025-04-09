db3a3b0eb10ace1244907eb8589961d7201ceb47
jarelsremy385 <jarelsremy385@gmail.com>
2025-04-09 21:42:22 +0000

    Add 63.cpp

---
+++ src/63.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.141592653589793;
+    float z = 10.0f; // Assuming a fixed precision for floating-point operations
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+
+    return 0;
+}
