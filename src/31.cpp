53500657ebb1c3c82c89c0e0bc2934da7679c7cc
jarelsremy385 <jarelsremy385@gmail.com>
2025-04-09 21:02:51 +0000

    Add 31.cpp

---
+++ src/31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    string name = "John";
+    float income = 500.0;
+    
+    // Your code here
+    
+    return 0;
+}
