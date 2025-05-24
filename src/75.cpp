1d2bdce31f1d263ae4e08d43a4413f322e1136f9
jarelsremy385 <jarelsremy385@gmail.com>
2025-05-24 09:17:28 +0000

    Add 75.cpp

---
+++ src/75.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = 20;
+    
+    if (a > b) {
+        cout << "The value of a is greater than b." << endl;
+    } else {
+        cout << "The value of a is not greater than b." << endl;
+    }
+    
+    if (c == 20) {
+        cout << "C is equal to 20." << endl;
+    } else {
+        cout << "C is not equal to 20." << endl;
+    }
+    
+    return 0;
+}
